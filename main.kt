7347b26ec53fdd56e2140d553f0c8e618f591846
hk-p <hukada.gogotea@gmail.com>
2021-03-21 22:46:17 +0900

    王様の会話を少し変更（特に意味はない）

--- main.kt
+++ main.kt
@@ -11,15 +11,16 @@ fun main() {
 }
 
 fun startingGame() {
-    king.talk("よくぞ来てくれた、勇者、、、")
-    king.talk("ええと、名前を何と言ったか、、")
+    king.talk("よくぞ来てくれた、デビルマン吉澤")
+    king.talk("え？、違う？、いやいや嘘を言っても無駄じゃよ、ブルガリアン坂本")
+    king.talk("ほぇ？、これも違う？、")
     king.talk("済まないが今一度名前を名乗ってはもらえないか")
 
     //名前を入力させる
     print("プレイヤーの名前を入力してください : ")
     val player = readLine()?.let { Player(it) }
 
-    king.talk("おお！、そうであったそうであった、いや覚えておったよ？ただちょっと今あのその...")
+    king.talk("ああ！、そうそう、そうだそうだ、あーいやね、覚えてたよ？覚えてたんだけどいやそのｱﾉｴｯﾄ...")
     if (player != null) {
         king.talk("ゴッホン...では改めて、勇者${player.name}よ、")
     }
